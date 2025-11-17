23554629456602a55f3833e0c6e11ef85d4909af
Johannes Karnatz <641_000004905@tln.bfw-hamburg.de>
2025-11-17 15:29:44 +0100

    logics

--- script/interpreter_logik.js
+++ script/interpreter_logik.js
@@ -6,4 +6,47 @@ const input = []
 const output = []
 
 
+// logic for the brainfuck translation or interpretation
 
+for (let el of array) {
+
+    switch (array[pos]) {
+
+        case ">" && array[pos] === (array.length - 1):
+        array.push(0);
+        pos++;
+        break;
+
+        case ">":
+            pos++;
+            break;
+
+        case "<" && array[pos] === 0:
+            array.unshift(0);
+            break;
+
+        case "<":
+            pos--;
+            break;
+
+        case "+":
+            array[pos]++
+            break;
+        
+        case "-":
+            array[pos]---;
+            break;
+
+        case ".":
+            Document.writeln(array.pos); // has to be changed with another function, that looks up the character from an ASCII table
+    
+        case ",":
+            // takes an input and transforms it into a decimal value from the ASCII table
+
+        case "[":
+            "while (array[pos] !== 0) {";
+
+        case "]":
+            "}"
+        }
+}
