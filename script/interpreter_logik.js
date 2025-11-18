ab9a1275d421505cdec4d5f70953e081c9cb9f38
Johannes Karnatz <641_000004905@tln.bfw-hamburg.de>
2025-11-18 15:16:48 +0100

    finished!!!!!!!

--- script/interpreter_logik.js
+++ script/interpreter_logik.js
@@ -1,52 +1,88 @@
-// variables
-const memory_size = 30000
-let array = new Array(memory_size).fill(0)
-let pos = 0
-const input = []
-const output = []
+// variables and constants
 
+document.getElementById('run-code').addEventListener('click', function(event) {
+    event.preventDefault();
+    let code = document.getElementById('code-input').value;
+    let userInput = document.getElementById('user-input').value;
+    let codeTokenArray = createTokenArray(code);
+    let userInputArray = createInputArray(userInput);
+    let output = becomeBrainfucked(codeTokenArray, userInputArray);
+    document.getElementById('output').textContent = output;
+});    
+
+function createInputArray(userInput) { 
+    return userInput.split(" ");
+}
+
+function createTokenArray(code) {
+    return code.split("");
+}
 
 // logic for the brainfuck translation or interpretation
+function becomeBrainfucked(codeTokenArray, userInputArray) {
+    let array = new Array(30000).fill(0);
+    let pos = 0;
+    let output = "";
+    let inputPos = 0;                
 
-for (let el of array) {
+    for (let i = 0; i < codeTokenArray.length; i++) {
+        switch (codeTokenArray[i]) {
+            case ">":
+                if (pos === array.length - 1) {
+                    array.push(0);
+                    pos++;
+                } else {
+                    pos++;
+                }
+                break;
 
-    switch (array[pos]) {
+            case "<":
+                if (pos === 0) {
+                    array.unshift(0);
+                } else {
+                    pos--;
+                }
+                break;
 
-        case ">" && array[pos] === (array.length - 1):
-        array.push(0);
-        pos++;
-        break;
+            case "+":
+                array[pos]++;
+                break;
+            
+            case "-":
+                array[pos]--;
+                break;
 
-        case ">":
-            pos++;
-            break;
-
-        case "<" && array[pos] === 0:
-            array.unshift(0);
-            break;
-
-        case "<":
-            pos--;
-            break;
-
-        case "+":
-            array[pos]++
-            break;
+            case ".":
+                output += String.fromCharCode(array[pos])
+                break;
         
-        case "-":
-            array[pos]---;
+            case ",":
+                array[pos] = userInputArray[inputPos] ? userInputArray[inputPos].charCodeAt(0):0;
+                inputPos++;
+                break;
+
+            case "[":
+                if (array[pos] === 0) {
+                    let open = 1;
+                    while (open > 0) {
+                        i++;
+                        if (codeTokenArray[i] === '[') open++;
+                        if (codeTokenArray[i] === ']') open--;
+                    }
+                }
+                break;
+
+            case "]":
+                if (array[pos] !== 0) {
+                    let close = 1;
+                    while (close > 0) {
+                        i--;
+                        if (codeTokenArray[i] === ']') close++;
+                        if (codeTokenArray[i] === '[') close--;
+                    }
+                }
             break;
-
-        case ".":
-            Document.writeln(array.pos); // has to be changed with another function, that looks up the character from an ASCII table
-    
-        case ",":
-            // takes an input and transforms it into a decimal value from the ASCII table
-
-        case "[":
-            "while (array[pos] !== 0) {";
-
-        case "]":
-            "}"
         }
+    }
+    return output;
 }
